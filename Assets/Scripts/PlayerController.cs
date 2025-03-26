8f86a2a861d19c2fc646f590843f11cbff2e2a3f
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2025-03-26 22:04:51 +1100

    changed privatevoidfix to update regulary

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,84 +8,92 @@ using TMPro;
 public class PlayerController : MonoBehaviour
 {
 
-private Rigidbody rb; 
-private int count;
-private float movementX;
-private float movementY;
- 
-public TextMeshProUGUI countText;
-public GameObject winTextObject;
-public float speed;
-public float hop;
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    public float speed;
+    public float hop;
 
 
 
- void Start()
-   {
-      SetCountText();
-      Destroy(GameObject.FindGameObjectWithTag("Enemy"));
-      rb = GetComponent<Rigidbody>();
-      count = 0;
-      winTextObject.SetActive(false);
-   }
+    void Start()
+    {
+        SetCountText();
+        Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        winTextObject.SetActive(false);
+    }
 
- void OnMove(InputValue movementValue)
+    void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
-        movementX = movementVector.x; 
-        movementY = movementVector.y; 
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
-   
-   
-   void SetCountText() 
+
+
+    void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        
-         if (count >= 20)
+
+        if (count >= 20)
         {
             winTextObject.SetActive(true);
         }
 
     }
- private void FixedUpdate() 
+
+    void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
-
-        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
-
-        rb.AddForce(movement * speed);
-
-        GetComponent<Rigidbody>().AddForce (movement * speed * Time.deltaTime); 
+        bool shouldJump = GetComponent<Rigidbody>().transform.position.y <= 1.6250001f;
+        if (Input.GetKeyDown("space") && shouldJump)
         {
-            if (Input.GetKeyDown ("space") && GetComponent<Rigidbody>().transform.position.y <= 6.6250001f) {
-                Vector3 jump = new Vector3 (0.0f, 200.0f, 0.0f);
+            Vector3 jump = new Vector3(0.0f, 200.0f, 0.0f);
 
-                GetComponent<Rigidbody>().AddForce (jump);
-            }
+            GetComponent<Rigidbody>().AddForce(jump);
         }
     }
 
- void OnTriggerEnter(Collider other) 
-   {
-      if (other.gameObject.CompareTag("PickUp")) 
-      {
-         other.gameObject.SetActive(false);
-         count = count + 1;
-         SetCountText();
-      }
-   }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-   private void OnCollisionEnter(Collision collision)
-{
-   if (collision.gameObject.CompareTag("EnemyObject"))
-   {
-       // Destroy the current object
-       Destroy(gameObject); 
-       // Update the winText to display "You Lose!"
-       winTextObject.gameObject.SetActive(true);
-       winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
-   }
-}
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("EnemyObject"))
+        {
+            // Destroy the current object
+            Destroy(gameObject);
+            // Update the winText to display "You Lose!"
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        }
+    }
 
 }
